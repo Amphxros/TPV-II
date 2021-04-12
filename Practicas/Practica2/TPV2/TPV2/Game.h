aec932c38f5c850ff0ea13e077c8028e7be2f64a
Jorzuiz <jordazurdo@hotmail.com>
2021-04-12 17:36:46 +0200

    Inicio de Practica 2 y diapos

---
+++ Practicas/Practica2/TPV2/TPV2/Game.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "sdlutils/InputHandler.h"
+#include "sdlutils/macros.h"
+#include "sdlutils/SDLUtils.h"
+class Manager;
+class Game
+{
+public:
+	Game();
+	~Game();
+
+	void init();
+	void run();
+
+private:
+	void update();
+	void render();
+
+	void createPlayer();
+	void createGameManager();
+
+	Manager* mngr_;
+	SDLUtils* sdl;
+	
+	SDL_Renderer* renderer;
+};
+
