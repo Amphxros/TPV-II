2da91df5fea3d1fcece9ec9a32c9a0a477f9c228
Jorzuiz <jordazurdo@hotmail.com>
2021-05-13 16:50:13 +0200

    Plantilla NetworkManager

--- Practicas/Practica3/TPV2/TPV2/NetworkSystem.cpp
+++ Practicas/Practica3/TPV2/TPV2/NetworkSystem.cpp
@@ -1,8 +1,9 @@
 #include "NetworkSystem.h"
 
-NetworkSystem::NetworkSystem(const char* host, Uint16 port, std::string playerName)
-{
-}
+NetworkSystem::NetworkSystem(const char* host, Uint16 port, std::string playerName): 
+	System(ecs::NetWorkSys),host_(host), port_(port), isMaster_(false), //
+	isGameReady_(false), id_(0), conn_(), p_(nullptr), m_(nullptr), //
+	otherPlayerAddress_(), localPlayerName_(playerName), remotePlayerName_("N/A"), lastTimeActive_(0) { }
 
 NetworkSystem::~NetworkSystem()
 {
@@ -10,6 +11,39 @@ NetworkSystem::~NetworkSystem()
 
 void NetworkSystem::init()
 {
+	if (SDLNet_Init() < 0)
+		throw "ERROR NOT CONNECTED";
+
+	p_ = SDLNet_AllocPacket(100);
+	m_ = reinterpret_cast<msg::Message*>(p_->data);
+
+	if(host_==nullptr){
+		isMaster_ = true;
+		id_ = 0;
+		conn_ = SDLNet_UDP_Open(port_);
+		if (!conn_)
+			throw "ERROR CATASTROFICO";
+
+		names_[0] = localPlayerName_;
+		names_[1] = remotePlayerName_;
+	}
+	else{
+		isMaster_ = false;
+		id_ = 1;
+		conn_ = SDLNet_UDP_Open(0);
+		if(!conn_)
+			throw "ERROR CATASTROFICO";
+		std::cout << "Trying to connect to other player at " << host_ << ":"
+				<< port_ << std::endl;
+	
+		// resolve the host name into an IPAdrress
+		if (SDLNet_ResolveHost(&otherPlayerAddress_, host_, port_) < 0) {
+			throw SDLNet_GetError();
+		}
+
+
+	}
+
 }
 
 void NetworkSystem::update()
